b27a7a9daef7c872d78212bfa216054574f93e2a
Pablo Rosas <pablolidcorp@gmail.com>
2023-12-27 10:15:20 -0600

    controlador de login creado

--- src/main/java/com/usuarios/controllers/UsuarioController.java
+++ src/main/java/com/usuarios/controllers/UsuarioController.java
@@ -7,22 +7,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usuarios.Dtos.UsuarioCreateDto;
-
+import com.usuarios.Dtos.UsuarioDto;
 import com.usuarios.services.IUsuarioService;
 
-@RestController // http://localhost:8090/api/usuarios/btener-nombre
-@RequestMapping("/api/usuarios")
-public class UsuarioControlller {
+@RestController // http://localhost:8090/api/v1/usuarios
+@RequestMapping("/api/v1/usuarios")
+public class UsuarioController {
 
 	@Autowired
 	IUsuarioService usuarioService;
 
-	@PostMapping("create-usuario") // http://localhost:8090/api/usuarios/create-usuario
+	@PostMapping("crear-usuario") // http://localhost:8090/api/v1/usuarios/crear-usuario
 	public UsuarioCreateDto crearUsuario(@RequestBody UsuarioCreateDto usuario) {
-		
+
+		// esta pendiente el desarrollo de la interfaz de servicio
 		UsuarioCreateDto response = usuarioService.crearUsuario(usuario);
 
 		System.out.println(usuario.toString());
 		return usuario;
+	}
+
+	@PostMapping("/login") // http://localhost:8090/api/v1/usuarios/login
+	public String crearUsuario(@RequestBody UsuarioDto usuarioLogin) {
+		System.out.println(usuarioLogin.toString());
+		return "usuario resibido correctamente dejame evaluarlo.";
 	}
 }
---
+++ src/main/java/com/usuarios/servicesImpls/UsuariosServicesImpl.java
@@ -0,0 +1,26 @@
+package com.usuarios.servicesImpls;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.usuarios.Dtos.UsuarioCreateDto;
+import com.usuarios.Dtos.UsuarioDtoList;
+import com.usuarios.services.IUsuarioService;
+
+@Service
+public class UsuariosServicesImpl implements IUsuarioService{
+
+	@Override
+	public UsuarioCreateDto crearUsuario(UsuarioCreateDto nuevoUsuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UsuarioDtoList> listarUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
